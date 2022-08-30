faf85f8590e64fcc15290a81950e39af22a8e282
Nifyr <nintendofyr@gmail.com>
2022-08-30 23:52:09 +0200

    Sort hirc items by group and child/parent relation upon serialization

--- Wwise Bank Converter/Wwise.cs
+++ Wwise Bank Converter/Wwise.cs
@@ -518,6 +518,8 @@ namespace WwiseBankConverter
 
             public override IEnumerable<byte> Serialize()
             {
+                SortHircItems();
+
                 List<byte> b0 = new();
                 releasableHircItemCount = (uint)loadedItem.Count;
                 b0.AddRange(GetBytes(releasableHircItemCount));
@@ -529,6 +531,96 @@ namespace WwiseBankConverter
                 b.AddRange(base.Serialize());
                 b.AddRange(b0);
                 return b;
+            }
+
+            private void SortHircItems()
+            {
+                List<HircItem> newList = new();
+
+                Dictionary<uint, HircItem> hircLookup = new();
+                foreach (HircItem hi in loadedItem)
+                    hircLookup.Add(hi.id, hi);
+
+                List<HircItem> attenuation = new();
+                List<HircItem> media = new();
+                List<HircItem> events = new();
+
+
+                foreach (HircItem hi in loadedItem)
+                {
+                    if (hi is Attenuation)
+                    {
+                        attenuation.Add(hi);
+                        continue;
+                    }
+                    if (TryAddMediaItem(hi, media, hircLookup))
+                        continue;
+                    if (TryAddEventItem(hi, events, hircLookup))
+                        continue;
+                }
+
+                newList.AddRange(attenuation);
+                newList.AddRange(media);
+                newList.AddRange(events);
+
+                loadedItem = newList;
+            }
+
+            private bool TryAddMediaItem(HircItem hi, List<HircItem> media, Dictionary<uint, HircItem> hircLookup)
+            {
+                if (media.Contains(hi))
+                    return false;
+
+                if (hi is MusicTrack || hi is Sound); //Whoa! An empty statement!
+                else if (hi is MusicSegment ms)
+                    foreach (uint childID in ms.musicNodeParams.children.childIDs)
+                        TryAddMediaItem(hircLookup[childID], media, hircLookup);
+                else if (hi is MusicRanSeqCntr mrsc)
+                    foreach (uint childID in mrsc.musicTransNodeParams.musicNodeParams.children.childIDs)
+                        TryAddMediaItem(hircLookup[childID], media, hircLookup);
+                else if (hi is MusicSwitchCntr msc)
+                    foreach (uint childID in msc.musicTransNodeParams.musicNodeParams.children.childIDs)
+                        TryAddMediaItem(hircLookup[childID], media, hircLookup);
+                else if (hi is RanSeqCntr rsc)
+                    foreach (uint childID in rsc.children.childIDs)
+                        TryAddMediaItem(hircLookup[childID], media, hircLookup);
+                else if (hi is ActorMixer am)
+                    foreach (uint childID in am.children.childIDs)
+                        TryAddMediaItem(hircLookup[childID], media, hircLookup);
+                else if (hi is LayerCntr lc)
+                    foreach (uint childID in lc.children.childIDs)
+                        TryAddMediaItem(hircLookup[childID], media, hircLookup);
+                else if (hi is SwitchCntr sc)
+                    foreach (uint childID in sc.children.childIDs)
+                        TryAddMediaItem(hircLookup[childID], media, hircLookup);
+                else
+                    return false;
+
+                media.Add(hi);
+                return true;
+            }
+
+            private bool TryAddEventItem(HircItem hi, List<HircItem> events, Dictionary<uint, HircItem> hircLookup)
+            {
+                if (events.Contains(hi))
+                    return false;
+
+                if (hi is Action)
+                    events.Add(hi);
+                else if (hi is Event e)
+                {
+                    foreach (uint actionID in e.actionIDs)
+                    {
+                        HircItem action = hircLookup[actionID];
+                        if (!events.Contains(action))
+                            events.Add(action);
+                    }
+                    events.Add(hi);
+                }
+                else
+                    return false;
+
+                return true;
             }
         }
 
@@ -2225,6 +2317,8 @@ namespace WwiseBankConverter
 
             public IEnumerable<byte> Serialize()
             {
+                childIDs.Sort();
+
                 List<byte> b = new();
                 childsCount = (uint)childIDs.Count;
                 b.AddRange(GetBytes(childsCount));
@@ -3691,6 +3785,8 @@ namespace WwiseBankConverter
 
             public IEnumerable<byte> Serialize()
             {
+                nodes.Sort((o0, o1) => o0.key.CompareTo(o1.key));
+
                 List<byte> b = new();
                 b.AddRange(GetBytes(key));
                 if (level > 0)
@@ -3844,6 +3940,9 @@ namespace WwiseBankConverter
 
             public IEnumerable<byte> Serialize()
             {
+                srcIDs.Sort();
+                dstIDs.Sort();
+
                 List<byte> b = new();
                 srcCount = (uint)srcIDs.Count;
                 b.AddRange(GetBytes(srcCount));
